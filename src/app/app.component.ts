8867dfe6e6d6684c118a535c0a93daa7e2f3468d
hasnain <hasnainkhankqs@gmail.com>
2020-11-29 22:30:22 +0500

    project working fine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faSignInAlt , faSignOutAlt , faHome} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'libraryOppCoder';
+export class AppComponent implements OnInit {
+  title : string = 'libraryOppCoder';
+  
+  faSignIn = faSignInAlt;
+  faSignOut = faSignOutAlt;
+  faHome = faHome;
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+  }
+  
 }
