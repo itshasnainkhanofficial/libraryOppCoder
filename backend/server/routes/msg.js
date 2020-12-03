19b6da90316eeeb13b73319d680e125bf293ebb8
hasnain <hasnainkhankqs@gmail.com>
2020-12-03 23:39:23 +0500

    module2  sending mail to email address

--- backend/server/routes/msg.js
+++ backend/server/routes/msg.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const messageModal = require("../models/msg");
+const nodemailer = require("nodemailer");
+
+
+let transporter = nodemailer.createTransport({
+    host: process.env.HOST,
+    port: process.env.HOST_PORT,
+    secure: process.env.HOST_SECURITY, 
+    auth: {
+      user: process.env.ADD, 
+      pass: process.env.PASS, 
+    },
+
+  });
+
 
 
 //post request 
@@ -9,9 +23,31 @@ router.post("/sendMsg", (req, res, next) => {
     const reqMsg = req.body;
 
     const mail = new messageModal({ name: reqMsg.name, email: reqMsg.email , message : reqMsg.message });
-    mail.save().then((data) => {
 
-        res.json({ data })
+    mail.save().then((data) => {
+        // res.json({ data })
+       transporter.sendMail({
+           subject: data.name,
+            from: data.email,
+            to: process.env.ADD,
+            html: data.message
+          }
+          , (err , data) => {
+
+            if(err) {
+        
+              res.json({status: 'msg sending fail'})
+                console.log("fail")
+            } 
+            else {
+        
+              res.json({status: 'msg success' , data : data})
+                console.log("passed")
+            }
+        
+          }
+          
+          )
 
     }).catch((err) => {
         err.whereError = "Error occured in saving msg";
