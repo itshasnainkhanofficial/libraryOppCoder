cd8868de6907a35aeb265e49b6cb0e72213b255a
hasnain <hasnainkhankqs@gmail.com>
2020-12-04 13:06:42 +0500

    getting mail name back to form by selctor

--- src/app/layout/contactus/contactus.component.ts
+++ src/app/layout/contactus/contactus.component.ts
@@ -1,23 +1,32 @@
 import { AfterViewChecked, AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, TemplateRef, ViewChild } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 import { LibraryState } from 'src/app/store';
 import { sendingCustomerSupportMessage } from 'src/app/store/actions/customer-support.actions';
 import { CustomerMessage } from 'src/app/shared/models/customer-message';
 import { ApiService } from 'src/app/services/api.service';
+import { Observable } from 'rxjs';
+import { selectFeatureName } from 'src/app/store/selectors/customer-support.selectors';
 
 @Component({
   selector: 'app-contactus',
   templateUrl: './contactus.component.html',
   styleUrls: ['./contactus.component.scss']
 })
-export class ContactusComponent implements AfterViewInit  {
+export class ContactusComponent implements AfterViewInit , OnInit  {
 
   phone = faPhone;
 
+  name$!: Observable<string>;
+
   @ViewChild("contactModal") ContactModal!: TemplateRef<any>;
 
+  ngOnInit(): void {
+
+    this.name$ = this.store.pipe(select(selectFeatureName))
+
+  }
   ngAfterViewInit(): void {
       
         this.modalRef = this.modalService.show(this.ContactModal);
