c0f878651e30055f05071e6a66026de08281aa49
hasnain <hasnainkhankqs@gmail.com>
2020-12-19 20:39:42 +0500

    make it better

--- backend/database/db.js
+++ backend/database/db.js
@@ -6,7 +6,7 @@ const mongoose  = require('mongoose');
 mongoose.connect(process.env.DB_URI , { useNewUrlParser : true , useUnifiedTopology: true })
 .then((db) => console.log("database connected succesfully"))
 .catch((err) => { 
-console.log("mongodb database connection failed");
+console.log("mongodb database connection failed" , err);
 process.exit(1);
 });
 
